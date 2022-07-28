098a1db3a880da9599b7d200bbb39bde8391ed38
lewi01 <isslewi03@gmail.com>
2022-07-29 00:40:53 +0300

    security update

--- src/main/java/com/lewisCode/hostelbookingsystem/configuration/WebConfiguration.java
+++ src/main/java/com/lewisCode/hostelbookingsystem/configuration/WebConfiguration.java
@@ -1,45 +1,39 @@
 package com.lewisCode.hostelbookingsystem.configuration;
 
 import com.lewisCode.hostelbookingsystem.role.Role;
-import com.lewisCode.hostelbookingsystem.service.UserServiceImpl;
-import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
-@AllArgsConstructor
-public class WebConfiguration  extends WebSecurityConfigurerAdapter {
-    private final PasswordEncoder passwordEncoder;
-    private final UserServiceImpl userDetailsService;
+public class WebConfiguration {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.httpBasic()
                 .and()
                 .csrf().disable().headers().frameOptions().disable()
                 .and()
                 .authorizeRequests()
+                .antMatchers("/api/user/signup").anonymous()
                 .antMatchers(HttpMethod.POST,"/api/admin/create").hasRole(Role.ADMIN.name())
-                .antMatchers("/api/user/signup").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        ;
-        // enable basic authentication
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);// enable basic authentication
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
